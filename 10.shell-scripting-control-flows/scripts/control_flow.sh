08517e1b7867c8df0beb30b0feb180ceed28d7be
olat95 <samsontunj@gmail.com>
2025-11-02 10:07:03 +0100

    completed task 9 [shell-scripting-comments] and task 10 [shell-scripting-control-flow]

---
+++ 10.shell-scripting-control-flows/scripts/control_flow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter a number: " num
+
+echo "You have entered the number $num"
+
+if [ $num -gt 0 ]; then
+    echo "The number is positive."
+
+elif [ $num -lt 0 ]; then
+    echo "The number is negative."
+    
+else
+    echo "The number is zero."
+fi
